184ea72d2b550fbf9d928a89c6fb430e856341df
Anton <abeschastnov@diasoft.ru>
2026-05-19 23:08:35 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/app/app.go
+++ hw12_13_14_15_16_calendar/internal/app/app.go
@@ -2,25 +2,67 @@ package app
 
 import (
 	"context"
+	"time"
+
+	"github.com/Anton-Beschastnov/GoDiasoft/internal/storage"
 )
 
-type App struct { // TODO
+type App struct {
+	logger  Logger
+	storage Storage
 }
 
-type Logger interface { // TODO
+type Logger interface {
+	Debug(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
+	Error(msg string, args ...any)
 }
 
-type Storage interface { // TODO
+type Storage interface {
+	CreateEvent(ctx context.Context, event storage.Event) error
+	UpdateEvent(ctx context.Context, event storage.Event) error
+	DeleteEvent(ctx context.Context, id string) error
+	GetEvent(ctx context.Context, id string) (storage.Event, error)
+	ListEventsForDay(ctx context.Context, userID string, date time.Time) ([]storage.Event, error)
+	ListEventsForWeek(ctx context.Context, userID string, startDate time.Time) ([]storage.Event, error)
+	ListEventsForMonth(ctx context.Context, userID string, startDate time.Time) ([]storage.Event, error)
 }
 
 func New(logger Logger, storage Storage) *App {
-	return &App{}
+	return &App{
+		logger:  logger,
+		storage: storage,
+	}
 }
 
-func (a *App) CreateEvent(ctx context.Context, id, title string) error {
-	// TODO
-	return nil
-	// return a.storage.CreateEvent(storage.Event{ID: id, Title: title})
+func (a *App) CreateEvent(ctx context.Context, event storage.Event) error {
+	a.logger.Debug("creating event", "id", event.ID, "title", event.Title)
+	return a.storage.CreateEvent(ctx, event)
 }
 
-// TODO
+func (a *App) UpdateEvent(ctx context.Context, event storage.Event) error {
+	a.logger.Debug("updating event", "id", event.ID)
+	return a.storage.UpdateEvent(ctx, event)
+}
+
+func (a *App) DeleteEvent(ctx context.Context, id string) error {
+	a.logger.Debug("deleting event", "id", id)
+	return a.storage.DeleteEvent(ctx, id)
+}
+
+func (a *App) GetEvent(ctx context.Context, id string) (storage.Event, error) {
+	return a.storage.GetEvent(ctx, id)
+}
+
+func (a *App) ListEventsForDay(ctx context.Context, userID string, date time.Time) ([]storage.Event, error) {
+	return a.storage.ListEventsForDay(ctx, userID, date)
+}
+
+func (a *App) ListEventsForWeek(ctx context.Context, userID string, startDate time.Time) ([]storage.Event, error) {
+	return a.storage.ListEventsForWeek(ctx, userID, startDate)
+}
+
+func (a *App) ListEventsForMonth(ctx context.Context, userID string, startDate time.Time) ([]storage.Event, error) {
+	return a.storage.ListEventsForMonth(ctx, userID, startDate)
+}
--- hw12_13_14_15_16_calendar/internal/logger/logger.go
+++ hw12_13_14_15_16_calendar/internal/logger/logger.go
@@ -1,20 +1,61 @@
 package logger
 
-import "fmt"
+import (
+	"io"
+	"log/slog"
+	"os"
+	"strings"
+)
 
-type Logger struct { // TODO
+type Logger struct {
+	logger *slog.Logger
+	level  slog.Level
 }
 
-func New(level string) *Logger {
-	return &Logger{}
+func New(level string, output io.Writer) *Logger {
+	if output == nil {
+		output = os.Stdout
+	}
+
+	lvl := parseLevel(level)
+	opts := &slog.HandlerOptions{
+		Level: lvl,
+	}
+
+	handler := slog.NewTextHandler(output, opts)
+	return &Logger{
+		logger: slog.New(handler),
+		level:  lvl,
+	}
 }
 
-func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+func parseLevel(level string) slog.Level {
+	switch strings.ToLower(level) {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn", "warning":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
 }
 
-func (l Logger) Error(msg string) {
-	// TODO
+func (l *Logger) Debug(msg string, args ...any) {
+	l.logger.Debug(msg, args...)
 }
 
-// TODO
+func (l *Logger) Info(msg string, args ...any) {
+	l.logger.Info(msg, args...)
+}
+
+func (l *Logger) Warn(msg string, args ...any) {
+	l.logger.Warn(msg, args...)
+}
+
+func (l *Logger) Error(msg string, args ...any) {
+	l.logger.Error(msg, args...)
+}
